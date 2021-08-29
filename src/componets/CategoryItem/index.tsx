b8ed70d8bce33a7273b6a151628f92923719c9f9
Eduard <eduardgrigoryan1993@gmail.com>
2021-08-29 15:41:22 +0400

    add style to p

--- src/componets/CategoryItem/index.tsx
+++ src/componets/CategoryItem/index.tsx
@@ -1,15 +1,20 @@
 import {observer} from "mobx-react";
 import {ICategory} from "../../stores/CategoryStore";
+import './index.scss';
 
 type OnSelect = (a: string) => void;
 
 interface IProps {
     category: ICategory;
     onSelect: OnSelect;
+    isSelected: boolean
 }
 
-const CategoryItem = observer(({category, onSelect}: IProps) => {
-    return (<p onClick={() => onSelect(category.id.toString())}>{category.name.toUpperCase()}</p>);
+const CategoryItem = observer(({category, onSelect, isSelected}: IProps) => {
+    console.log(isSelected);
+
+    return (<p className={isSelected ? 'selected' : '' }
+               onClick={() => onSelect(category.id.toString())}>{category.name.toUpperCase()}</p>);
 });
 
 
--- src/componets/CategoryList/index.tsx
+++ src/componets/CategoryList/index.tsx
@@ -1,9 +1,11 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {observer} from "mobx-react";
 import CategoryItem from "../CategoryItem";
 import {useStores} from "../../use-stores";
 
 const CategoryList = observer(() => {
+    const [selectedId, changeSelectedId] = useState('');
+
     const {categoryStore, galleryStore} = useStores();
 
     useEffect(() => {
@@ -11,6 +13,7 @@ const CategoryList = observer(() => {
     }, [categoryStore]);
 
     function onSelected(id: string) {
+        changeSelectedId(id);
         galleryStore.fetchByCategoryId(id);
     }
 
@@ -19,7 +22,8 @@ const CategoryList = observer(() => {
             <h2>Categories</h2>
             {categoryStore.data.length === 0 && <p>No Category</p>}
             {categoryStore.data.map(category =>
-                <CategoryItem key={category.id} category={category} onSelect={onSelected}/>)}
+                <CategoryItem key={category.id} category={category} onSelect={onSelected}
+                              isSelected={category.id.toString() === selectedId}/>)}
         </>
     );
 });
