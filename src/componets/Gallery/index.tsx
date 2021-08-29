954e0354072eff02c6f531c2f6519abf5b8a9001
Eduard <eduardgrigoryan1993@gmail.com>
2021-08-29 15:58:43 +0400

    add load more

--- src/componets/Gallery/index.tsx
+++ src/componets/Gallery/index.tsx
@@ -1,18 +1,36 @@
 import {observer} from "mobx-react";
 import ImageViewer from "../ImageViewer";
 import {useStores} from "../../use-stores";
+import {Button, Grid} from '@material-ui/core';
+import {Container} from "./style";
 
 const Gallery = observer(() => {
     const {galleryStore} = useStores();
 
     return (
-        <>
-            {galleryStore.data.length === 0 && <p>There isn't selected category</p>}
-            {galleryStore.data.length > 0 &&
-            <h1 style={{textAlign: 'center'}}>Your selected category
-                name: {galleryStore.data[0].categories[0].name}</h1>}
+        <Container>
+            <Grid item xs={12}>
+                {galleryStore.data.length === 0 && <p>There isn't selected category</p>}
+
+                {galleryStore.data.length > 0 &&
+                <h3>Your selected category name: {galleryStore.data[0].categories[0].name.toUpperCase()}</h3>}
+            </Grid>
+
+
             {galleryStore.data.map(item => <ImageViewer key={item.id} data={item}/>)}
-        </>
+
+            <Grid container
+                  direction="row"
+                  justifyContent="center"
+                  alignItems="center">
+                {galleryStore.data.length > 0 &&
+                <Button variant="contained" color="primary">
+                    Load more
+                </Button>}
+            </Grid>
+
+
+        </Container>
     );
 });
 
